c9dd020ea14579277c001d7382b15d284e14d8dc
typhon150 <bsupmac@gmail.com>
2024-10-06 15:45:50 +0200

    Debut d'interaction entre client et serveur

---
+++ src/main/java/fr/uga/l3miage/pc/Client.java
@@ -0,0 +1,45 @@
+package fr.uga.l3miage.pc;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class Client {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 12345)) {
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            Scanner scanner = new Scanner(System.in);
+            int nbTour = 0;
+            int numeroConnexion = in.readInt();
+            System.out.println("numero de connexion : "+numeroConnexion);
+            if(numeroConnexion == 1){
+                System.out.println("Combien de tours voulez vous jouer ?");
+                nbTour = parseInt(scanner.nextLine());
+                out.writeInt(nbTour);
+                System.out.println("nb tour envoyer au serveru : "+nbTour);
+            }
+            else if(numeroConnexion == 2){
+                nbTour = in.readInt();
+            }
+
+
+            for (int i = 0; i < nbTour; i++) {
+                System.out.println(" - Faites votre choix : 'coopérer' ou 'trahir' ?");
+                String choice = scanner.nextLine();
+                out.writeUTF(choice);
+
+                String resultat = in.readUTF();
+                System.out.println(resultat);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/fr/uga/l3miage/pc/Serveur.java
@@ -0,0 +1,81 @@
+package fr.uga.l3miage.pc;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Serveur {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(12345)) {
+            System.out.println("Serveur en attente des joueurs...");
+
+            // Connexion des deux joueurs
+            Socket player1 = serverSocket.accept();
+            System.out.println("Joueur 1 connecté.");
+            // Streams pour la communication du joueur 1
+            DataInputStream in1 = new DataInputStream(player1.getInputStream());
+            DataOutputStream out1 = new DataOutputStream(player1.getOutputStream());
+
+            out1.writeInt(1);
+            //Demande du nombre de tours au premier joueur
+            int nbTour = in1.readInt();
+            System.out.println("nb tours  : "+nbTour);
+
+            Socket player2 = serverSocket.accept();
+            System.out.println("Joueur 2 connecté.");
+            // Streams pour la communication du joueur 2
+            DataInputStream in2 = new DataInputStream(player2.getInputStream());
+            DataOutputStream out2 = new DataOutputStream(player2.getOutputStream());
+            out2.writeInt(2);
+            out2.writeInt(nbTour);
+
+            //variarbles pour ld reoulement du jeu
+            int score1 = 0;
+            int score2 = 0;
+
+            for(int i =0; i<nbTour; i++) {
+                // Récupérer les choix des joueurs
+                String choice1 = in1.readUTF();
+                String choice2 = in2.readUTF();
+                System.out.println("choix j1 : "+choice1);
+                System.out.println("choix j2 : "+choice2);
+
+
+                //Envoie des résultats
+                if(choice1.equals("trahir") && choice2.equals("trahir")){
+                    out1.writeUTF("Vous avez tous les deux trahis l'autre");
+                    out2.writeUTF("Vous avez tous les deux trahis l'autre");
+                    score1+=1;
+                    score2+=1;
+                }
+                else if(choice1.equals("cooperer") && choice2.equals("cooperer")){
+                    out1.writeUTF("Vous avez tous les deux coopérer");
+                    out2.writeUTF("Vous avez tous les deux coopérer");
+                    score1+=3;
+                    score2+=3;
+                }
+                else if(choice1.equals("trahir") && choice2.equals("cooperer")){
+                    out1.writeUTF("Tu as réussi a trahir le deuxième joueur");
+                    out2.writeUTF("Tu as été tarhis par le deuxième joueur");
+                    score1+=5;
+                }
+                else if(choice1.equals("cooperer") && choice2.equals("trahir")){
+                    out1.writeUTF("Tu as été tarhis par le deuxième joueur");
+                    out2.writeUTF("Tu as réussi a trahir le deuxième joueur");
+                    score2+=5;
+                }
+
+            }
+            System.out.println("score1 : "+score1);
+            System.out.println("score2 : "+score2);
+
+            // Fermer les connexions
+            player1.close();
+            player2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

d5b48362bd63dd5b905cdb360a85c73ab1d5d94e
jugurtao <jugurtaourzik@gmail.com>
2024-10-10 16:49:41 +0200

    +setup: main fucntion overall implemented

--- src/test/java/fr/uga/l3miage/pc/prisonersdilemma/PrisonersDilemmaApplicationTests.java
+++ src/test/java/fr/uga/l3miage/pc/prisonersdilemma/PrisonersDilemmaApplicationTests.java
@@ -1,9 +1,9 @@
 package fr.uga.l3miage.pc.prisonersdilemma;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class PrisonersDilemmaApplicationTests {
 
 	@Test

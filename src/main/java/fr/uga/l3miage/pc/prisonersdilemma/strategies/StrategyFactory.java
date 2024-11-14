3b95a3e003cd1f66a804ca2456698b4faf726d3f
monachov <vivian.monachon@etu.univ-grenoble-alpes.fr>
2024-11-14 22:55:57 +0100

    build(strategies): added strategy faactory

--- src/main/java/fr/uga/l3miage/pc/prisonersdilemma/strategies/StrategyFactory.java
+++ src/main/java/fr/uga/l3miage/pc/prisonersdilemma/strategies/StrategyFactory.java
@@ -1,5 +1,40 @@
 package fr.uga.l3miage.pc.prisonersdilemma.strategies;
 
+import java.util.Random;
+
 public class StrategyFactory {
-    //public Strategy createStrategy()
+    private static final Random random = new Random();
+
+    public static Strategy createRandomStrategy() {
+        int strategyIndex = random.nextInt(12); // Nombre de stratégies disponibles
+
+        switch (strategyIndex) {
+            case 0:
+                return new AlwaysBetray();
+            case 1:
+                return new AlwaysCooperate();
+            case 2:
+                return new NaivePeacemaker(random);
+            case 3:
+                return new Peacemaker(random);
+            case 4:
+                return new PollsterRandomBetray(random);
+            case 5:
+                return new RandomStrategy(random);
+            case 6:
+                return new ResentfulStrategy();
+            case 7:
+                return new TitforTat();
+            case 8:
+                return new TitforTatRandom(random);
+            case 9:
+                return new TitforTwoTats(random);
+            case 10:
+                return new TitforTwoTatsRandom(random);
+            case 11:
+                return new RepentantPollster(random);
+            default:
+                throw new IllegalStateException("Unexpected value: " + strategyIndex);
+        }
+    }
 }
